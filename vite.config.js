3b40f5b96485e5632a2b4988d30cfa95663b8165
Arnaud <arnaud.guillemin33@gmail.com>
2025-03-18 17:36:50 +0100

    Ajout d'images et mise à jour du code

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Stephane-Aknin.github.io/', 
    
   build: {
     minify: 'terser', 
